806d721feb9afee079c2337b21eb43a2f320acb3
antony <antony.leo12agosto@gmail.com>
2022-04-19 21:25:42 -0500

    not apiTurismo

--- apiTurismo/stores/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Lugar
-
-class LugarListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lugar
-        fields = ['id','name', 'description', 'link_img', 'region', 'departamento']
-
--- apiTurismo/stores/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import LugaresLisAPIView, LugarAPIView, LugarAPIViewByDepa
-
-urlpatterns = [ path('', LugaresLisAPIView.as_view(),name="lugares_list"), 
-    path('<int:id>/', LugarAPIView.as_view(), name="pizzeria_detail"),
-    path('departamento/<str:departamento>/', LugarAPIViewByDepa.as_view(), name="all_depas")
-
-]
